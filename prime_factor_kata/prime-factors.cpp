0c80f6ca0e39f5135487e375123ba111ed195006
noname <none@none.com>
2025-07-02 10:07:51 +0900

    initial working code for number 1-3

--- prime_factor_kata/prime-factors.cpp
+++ prime_factor_kata/prime-factors.cpp
@@ -1,5 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
 class PrimeFactors {
 public:
-private: 
+    vector<int> of(int number) {
+        vector<int> result = {};
+        if (2 == number) {
+            result.push_back(2);
+        }
+        if (3 == number) {
+            result.push_back(3);
+        }
+        return result;
+    }
 
+private: 
+    
 };
